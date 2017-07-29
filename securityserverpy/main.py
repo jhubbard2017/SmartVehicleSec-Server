2df0bab9b320c036a93e512d714a09c7ac7b835b
Jordan Hubbard <jordan_hubbard@apple.com>
2017-07-29 14:20:22 -0700

    Patched up RESTapi to collect device name upon sending back security data

--- securityserverpy/main.py
+++ securityserverpy/main.py
@@ -6,10 +6,14 @@
 import os
 import xmltodict
 from argparse import ArgumentParser
+from threading import Thread
+import sys
 
 from securityserverpy import _logger
 from securityserverpy.version import __version__
 from securityserverpy.securityserver import SecurityServer
+
+sec_server = None
 
 def _config_from_args():
     """sets up argparse to parse command line args
@@ -36,7 +40,6 @@ def _config_from_args():
 
     return parser.parse_args()
 
-
 def _config_logging():
     """allow log level to be set by environment"""
     if 'SECURITYSERVER_LOG' in os.environ.keys():
@@ -44,20 +47,29 @@ def _config_logging():
         _logger.setLevel(log_level)
         _logger.warn('set log level [{0}]'.format(log_level))
 
+def main_thread():
+    sec_server.start()
+
+# Make global so can be accessed when need to stop system, and safely save settings
+config = _config_from_args()
+http_port = int(config.http_port)
+udp_port = int(config.udp_port)
+sec_server = SecurityServer(host=config.host, http_port=http_port, udp_port=udp_port, no_hardware=config.no_hardware)
 
 def main():
     """ main function
 
     set up configs, connect to client, and secure their vehicle :)
     """
-    _config_logging()
-    config = _config_from_args()
-
-    http_port = int(config.http_port)
-    udp_port = int(config.udp_port)
-
-    sec_server = SecurityServer(host=config.host, http_port=http_port, udp_port=udp_port, no_hardware=config.no_hardware)
-    sec_server.start()
+    thread = Thread(target=main_thread)
+    thread.daemon = True
+    thread.start()
+    while True:
+        text = raw_input()
+        if text == "stop":
+            _logger.info("Shutting down. Saving settings.")
+            sec_server.save_settings()
+            sys.exit(0)
 
 if __name__ == '__main__':
     main()
