87bf301ab185ab65fab83ca7f40123a6eb4ceeed
Developer <developer@Jordans-MBP.attlocal.net>
2017-09-07 22:38:49 -0500

    Fixed error in code

--- securityserverpy/securityserver.py
+++ securityserverpy/securityserver.py
@@ -56,10 +56,10 @@ class SecurityServer(object):
             required data:
                 md_mac_address: str
             """
-            if not request.json or not 'md_mac_address' in request.json or not 'rd_mac_address' in request.json:
+            if not request.json or (not 'md_mac_address' in request.json) or (not 'rd_mac_address' in request.json):
                 _logger.debug("Error! Device not found in request data.")
                 abort(_FAILURE_CODE)
-            if not request.json['md_mac_address'] and request.json['rd_mac_address']:
+            if request.json['rd_mac_address']:
                 rd_mac_address = request.json['rd_mac_address']
             else:
                 md_mac_address = request.json['md_mac_address']
